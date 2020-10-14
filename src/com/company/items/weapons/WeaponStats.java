03c07df5cadea2ba20bfa428a70f9b6315b4b61a
tavinogradova92 <tavinogradova92@gmail.com>
2020-10-14 15:24:29 +0200

    Weapon stats added

---
+++ src/com/company/items/weapons/WeaponStats.java
@@ -0,0 +1,12 @@
+package com.company.items.weapons;
+
+public class WeaponStats {
+
+    public final static int meleeAttack = 15;
+    public final static int rangedAttack = 5;
+    public final static int magicAttack = 25;
+
+    public final static int meleeAttackLevelUp = 2;
+    public final static int rangedAttackLevelUp = 3;
+    public final static int magicAttackLevelUp = 2;
+}

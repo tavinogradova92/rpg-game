eadf1086c87e265ec2949ab640b4c7d1a7c50cae
tavinogradova92 <tavinogradova92@gmail.com>
2020-10-14 15:29:11 +0200

    Weapon interface created

---
+++ src/com/company/items/weapons/Weapon.java
@@ -0,0 +1,10 @@
+package com.company.items.weapons;
+
+public interface Weapon {
+    public int countAttackPower();
+    public void setAttackDamage(int modify);
+    public String weaponType();
+    public void setLevel(int Level);
+    public int getLevel();
+    public String getName();
+}

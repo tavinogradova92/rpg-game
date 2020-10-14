1622a99857d66591c0fab1355c7652e5dc72fae8
tavinogradova92 <tavinogradova92@gmail.com>
2020-10-14 14:45:52 +0200

    Creating basic stats for different types of heroes

---
+++ src/com/company/heroes/HeroLevelStats.java
@@ -0,0 +1,36 @@
+package com.company.heroes;
+
+public class HeroLevelStats {
+
+    // initial stats
+    public final static int warriorHealth = 150;
+    public final static int warriorStrength = 10;
+    public final static int warriorDexterity = 3;
+    public final static int warriorIntelligence = 1;
+
+    public final static int rangerHealth = 120;
+    public final static int rangerStrength = 5;
+    public final static int rangerDexterity = 10;
+    public final static int rangerIntelligence = 2;
+
+    public final static int mageHealth = 100;
+    public final static int mageStrength = 2;
+    public final static int mageDexterity = 3;
+    public final static int mageIntelligence = 10;
+
+    // on level up
+    public final static int warriorHealthLevelUp = 30;
+    public final static int warriorStrengthLevelUp = 5;
+    public final static int warriorDexterityLevelUp = 2;
+    public final static int warriorIntelligenceLevelUp = 1;
+
+    public final static int rangerHealthLevelUp = 20;
+    public final static int rangerStrengthLevelUp = 2;
+    public final static int rangerDexterityLevelUp = 5;
+    public final static int rangerIntelligenceLevelUp = 1;
+
+    public final static int mageHealthLevelUp = 15;
+    public final static int mageStrengthLevelUp = 1;
+    public final static int mageDexterityLevelUp = 2;
+    public final static int mageIntelligenceLevelUp = 5;
+}
--- src/com/company/heroes/HeroTypes.java
+++ src/com/company/heroes/HeroTypes.java
@@ -1,7 +1,7 @@
 package com.company.heroes;
 
 public enum HeroTypes {
-    MAGE,
     WARRIOR,
-    RANGER
+    RANGER,
+    MAGE
 }
